c1e2bde16e6cd6d7f21b7f88d722825c1c858593
Vitor Gouveia <vitorbgouveia@gmail.com>
2020-11-21 01:06:31 -0300

    feat(commit-initial)

---
+++ user_seed.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+      "username": "admin",
+      "email": "admin@email.com.br",
+      "firstName": "admin",
+      "lastName": "",
+      "node_id": "http://kong:8001",
+      "admin": true,
+      "active" : true,
+      "password": "root"
+  }
+]
